b1522334e9f0f9e71dfdbe9fa12e3679134f8380
Nikita Gladkikh <gladkikh.nikita@gmail.com>
2019-08-10 16:35:58 +0300

    Review startup.py

--- start_bot.py
+++ start_bot.py
@@ -1,5 +1,6 @@
+# If function is oneline only there is no need in this function.
 def main():
-    import setup
+    import setup # import goes first in file.
     setup.client.run_until_disconnected()
 if __name__ == '__main__':
     main()
